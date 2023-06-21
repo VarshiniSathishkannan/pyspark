49bb3a521150a589c125a668428584ab98d3039b
VarshiniSathishkannan <varshinisathish2112@gmail.com>
2023-06-21 21:56:02 +0530

    Kafka

--- producer.py
+++ producer.py
@@ -5,6 +5,15 @@ from kafka import KafkaProducer
 producer = KafkaProducer(bootstrap_servers=['localhost:9092'],
                          value_serializer=lambda x: 
                          dumps(x).encode('utf-8'))
+
+# value serializer is for converting the int or other datatypes to a stream of bytes
+# Kafka sends only stream of bytes
+# json.dumps() function will convert a subset of Python objects into a json string. 
+# Not all objects are convertible and you may need to create a dictionary of data you wish to expose before serializing to JSON
+# Passing the Python dictionary to json.dumps() function will return a string. 
+# The encode() method encodes the string, using the specified encoding. 
+# If no encoding is specified, UTF-8 will be used.
+
 for e in range(1000):
     data = {'number' : e}
     producer.send('first_kafka_topic', value=data)
